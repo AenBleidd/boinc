7e55caf2a428aa755dd9c3db26a50d458c9f229d
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2019-12-12 17:14:08 +0100

    Remove unnecessary deprecation flags
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- lib/boinc_win.h
+++ lib/boinc_win.h
@@ -115,20 +115,6 @@
 // Windows System Libraries
 //
 
-// Visual Studio 2005 has extended the C Run-Time Library by including "secure"
-// runtime functions and deprecating the previous function prototypes.  Since
-// we need to use the previous prototypes to maintain compatibility with other
-// platforms we are going to disable the deprecation warnings if we are compiling
-// on Visual Studio 2005
-#if _MSC_VER >= 1400
-#ifndef _CRT_SECURE_NO_DEPRECATE
-#define _CRT_SECURE_NO_DEPRECATE
-#endif
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-#endif
-
 // Target Windows XP or better with Internet Explorer 5.01 or better
 #ifndef WINVER
 #define WINVER 0x0501
