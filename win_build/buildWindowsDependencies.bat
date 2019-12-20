066f2a419a865e0e0d41cfee1bd26b457bad0409
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2019-12-20 17:42:47 +0100

    Add OpenSSL
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- win_build/buildWindowsDependencies.bat
+++ win_build/buildWindowsDependencies.bat
@@ -1,3 +1,12 @@
+@echo off
+REM Required tools:
+REM Visual Studio 2013 with Windows XP (v120_xp)
+REM 7-zip
+REM curl
+REM tar
+REM perl
+REM nasm
+
 set dependencies_root_name=boinc_depends_win_vs2013
 set dependencies_root_location=../../
 set dependencies_root=%dependencies_root_location%%dependencies_root_name%
@@ -59,10 +68,100 @@ REM cd %win_build_path%
 REM OpenSSL
 
 if not exist %dependencies_root%/openssl.tar.gz (
-    curl -L https://www.openssl.org/source/old/0.9.x/openssl-0.9.8e.tar.gz --output %dependencies_root%/openssl.tar.gz
+    curl -L https://www.openssl.org/source/old/1.0.2/openssl-1.0.2a.tar.gz --output %dependencies_root%/openssl.tar.gz
 )
 
 if exist %dependencies_root%/openssl.tar.gz (
     tar xzvf %dependencies_root%/openssl.tar.gz -C %dependencies_root%
-    mv %dependencies_root%/openssl-0.9.8e %dependencies_root%/openssl
+    mv %dependencies_root%/openssl-1.0.2a %dependencies_root%/openssl
 )
+
+if %platform%==Win32 (
+    cp patches/OpenSSL_debug.patch %dependencies_root%
+    if %configuration%==Debug (
+        cp patches/OpenSSL_nmake_debug.patch %dependencies_root%
+    )
+    if %configuration%==Release (
+        cp patches/OpenSSL_nmake_release.patch %dependencies_root%
+    )
+
+    cd %dependencies_root%/openssl
+
+    if %configuration%==Debug (
+        git --work-tree=. --git-dir=.git apply ../OpenSSL_debug.patch
+    )
+
+    perl Configure VC-WIN32
+    call ms/do_nasm
+
+    REM https://devblogs.microsoft.com/cppblog/windows-xp-targeting-with-c-in-visual-studio-2012/
+    if %configuration%==Debug (
+        git --work-tree=. --git-dir=.git apply ../OpenSSL_nmake_debug.patch
+    )
+
+    if %configuration%==Release (
+        git --work-tree=. --git-dir=.git apply ../OpenSSL_nmake_release.patch
+    )
+
+    nmake -f ms/ntdll.mak
+
+    mv out32dll.dbg out32dll
+    rmdir /s /q tmp32dll.dbg
+
+)
+
+if not exist mswin/%platform%/%configuration%/lib (
+    mkdir mswin\%platform%\%configuration%\lib
+)
+
+if not exist mswin/%platform%/%configuration%/bin (
+    mkdir mswin\%platform%\%configuration%\bin
+)
+
+cp out32dll/libeay32.dll mswin/%platform%/%configuration%/bin/
+cp out32dll/libeay32.pdb mswin/%platform%/%configuration%/bin/
+cp out32dll/ssleay32.dll mswin/%platform%/%configuration%/bin/
+cp out32dll/ssleay32.pdb mswin/%platform%/%configuration%/bin/
+
+cp out32dll/libeay32.exp mswin/%platform%/%configuration%/lib/
+cp out32dll/libeay32.lib mswin/%platform%/%configuration%/lib/
+cp out32dll/ssleay32.exp mswin/%platform%/%configuration%/lib/
+cp out32dll/ssleay32.lib mswin/%platform%/%configuration%/lib/
+cp ms/libeay32.def mswin/%platform%/%configuration%/lib/
+cp ms/ssleay32.def mswin/%platform%/%configuration%/lib/
+
+cp README mswin/
+
+rename \\.\%CD%\nul. deletefile.txt
+
+del /q *
+rmdir /s /q apps
+rmdir /s /q bugs
+rmdir /s /q certs
+rmdir /s /q crypto
+rmdir /s /q demos
+rmdir /s /q doc
+rmdir /s /q engines
+rmdir /s /q inc32
+rmdir /s /q MacOS
+rmdir /s /q ms
+rmdir /s /q Netware
+rmdir /s /q os2
+rmdir /s /q out32dll
+rmdir /s /q perl
+rmdir /s /q shlib
+rmdir /s /q ssl
+rmdir /s /q test
+rmdir /s /q times
+rmdir /s /q tools
+rmdir /s /q util
+rmdir /s /q VMS
+
+mv mswin/README ./
+
+cd ../
+del openssl.tar.gz
+del OpenSSL_debug.patch
+del OpenSSL_nmake_debug.patch
+
+cd %win_build_path%
